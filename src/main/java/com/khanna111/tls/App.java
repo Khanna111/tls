910e11fa07fd853467d069b0db1906b99479e52f
Gaurav Khanna <gk2@hotmail.com>
2016-05-07 16:32:27 -0700

    adding spring boot support

--- src/main/java/com/khanna111/tls/App.java
+++ src/main/java/com/khanna111/tls/App.java
@@ -12,11 +12,13 @@ import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 
  * @author gkhanna
  */
+@SpringBootApplication
 public class App {
     private final static Logger LOGGER = LoggerFactory.getLogger(App.class);
 
